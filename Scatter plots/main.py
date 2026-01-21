ad273ecb334cf3eb3ed1a52cca15d9f427313a5a
Sof-Vi <sofia28vargas10@gmail.com>
2026-01-21 19:50:14 -0400

    Implement scatter plot in main.py
    Add scatter plot implementation using random data points.

---
+++ Scatter plots/main.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pylot as plt 
+#imports both mathplot and numpy which are neccesary for making the graph
+
+x = np.random.rand(15) 
+#Selects the points graphed on the x axis can be specified as well
+y = np.random.rand(15)
+#Selects the points graphed on the x axis can be specified as well
+
+plt.scatter(x,y, c= 'blue',  marker= 'H', s= 67)
+#controls the parameters for each thing, where c = color, marker = shape of point, s = size
+plt.show
+#Show where the marker should be
+
+#This is useful for data points since they are not treated like functions instead like individuals variables 
