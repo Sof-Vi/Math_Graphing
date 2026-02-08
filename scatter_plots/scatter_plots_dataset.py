be64587221ab3cdf2bc968aefa31984ffaf62408
Sof-Vi <sofia28vargas10@gmail.com>
2026-02-08 19:36:46 -0400

    fixed dataset and tested a graph on each

--- scatter_plots/scatter_plots_dataset.py
+++ scatter_plots/scatter_plots_dataset.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt 
 #imports both mathplot and numpy which are neccesary for making the graph
 
-x = [200]
+x = [200, 200, 200, 200, 200, 200, 200]
 #Uses the proton run log data where the x axis represents the energy. 
 y = [1.29677E-11, 2.22845E-11, 4.51678E-11, 1.04395E-11, 8.63505E-11, 2.60134E-11, 2.33483E-11]
 #Uses the proton run log data where the y axis represent the FTF
 
-plt.scatter(x,y, c= 'blue',  marker= 'H', s= 67)
+plt.scatter(x, y, c= 'blue',  marker= 'H', s= 67)
 #controls the parameters for each thing, where c = color, marker = shape of point, s = size
-plt.show
+plt.show()
 #Show where the marker should be
 
 #This is useful for data points since they are not treated like functions instead like individuals variables 
--- scatter_plots/scatter_plots_random.py
+++ scatter_plots/scatter_plots_random.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 x = np.random.rand(15) 
 #Selects the points graphed on the x axis can be specified as well
 y = np.random.rand(15)
-#Selects the points graphed on the x axis can be specified as well
+#Selects the points graphed on the y axis can be specified as well
 
 plt.scatter(x,y, c= 'blue',  marker= 'H', s= 67)
 #controls the parameters for each thing, where c = color, marker = shape of point, s = size
-plt.show
+plt.show()
 #Show where the marker should be
 
 #This is useful for data points since they are not treated like functions instead like individuals variables 
